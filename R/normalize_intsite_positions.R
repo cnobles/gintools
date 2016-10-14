9d1896c9e6fbd65c2c62698332294ec8f7113c39
Christopher Nobles <cnobles@upenn.edu>
2016-10-14 12:14:33 -0400

    Added normalizing functions for unique and multihit sites.

---
+++ R/normalize_intsite_positions.R
@@ -0,0 +1,91 @@
+#' Normalize integration site positions between samples
+#'
+#' \code{normalize_intsite_positions} identifies sites within a gap distance and
+#' assigns each of the sites in the group the most frequent position as well as
+#' a positionID (pos.clus).
+#'
+#' @description Integration sites across multiple samples may be identified at
+#' slightly different nucleotide positions. This function identifies sites
+#' within the gap distance from eachother and normalized their position to the
+#' most frequent position. Similar to standardizing the integration sites, but
+#' not as logic heavy.
+#'
+#' @usage
+#' normalize_intsite_positions(sites)
+#'
+#' normalize_intsite_positions(sites, gap = 0L)
+#'
+#' @param sites GRanges object where each row represents one integration range
+#' or site.
+#'
+#' @param gap integer for the distance between sites to consider.
+#'
+#' @examples
+#'
+#' gr <- GRanges(
+#'   seqnames = rep("chr1", 10),
+#'   ranges = IRanges(
+#'     start = rnorm(10, 20, 1),
+#'     width = rep(15, 10)),
+#'   strand = rep("+", 10))
+#'
+#' normalize_intsite_positions(gr)
+#'
+#' normalize_intsite_positions(gr, gap = 1L)
+#'
+#' @author Christopher Nobles, Ph.D.
+#' @export
+
+normalize_intsite_positions <- function(sites, gap = 0L){
+  sites$called.pos <- ifelse(strand(sites) == "+", start(sites), end(sites))
+  sites$called.bp <- ifelse(strand(sites) == "+", end(sites), start(sites))
+  sites <- flank(sites, -1, start = TRUE)
+
+  graph.gap <- graph_clusters(sites, gap = gap)
+  clusters.gap <- clusters(graph.gap)
+  clusters_to_normalize <- grep(TRUE, clusters.gap$csize > 1)
+
+  sites$pos.clus <- clusters.gap$membership
+
+  norm.clus.dfl <- lapply(clusters_to_normalize, function(pos.clus){
+    obs <- grep(TRUE, sites$pos.clus == pos.clus)
+    dfr <- data.frame("strand" = strand(sites[obs]),
+                      "called.pos" = sites[obs]$called.pos,
+                      "pos.clus" = sites[obs]$pos.clus,
+                      stringsAsFactors = FALSE)
+    dfr
+  })
+
+  #Generate a key of the clusters and normalized positions
+  norm.key <- as.data.frame(bind_rows(lapply(norm.clus.dfl, function(pos.dfr){
+    pos.freq <- as.data.frame(table(pos.dfr$called.pos))
+    top.freq <- as.numeric(as.character(
+      pos.freq[pos.freq$Freq == max(pos.freq$Freq), "Var1"]
+    ))
+    norm.pos <- ifelse(unique(pos.dfr$strand) == "+", min(top.freq), max(top.freq))
+    key.row <- data.frame("norm.pos" = norm.pos,
+                          "pos.clus" = unique(pos.dfr$pos.clus),
+                          stringsAsFactors = FALSE)
+    key.row
+  })))
+  unnorm.sites <- grep(TRUE, !sites$pos.clus %in% clusters_to_normalize)
+  unnorm.key <- data.frame("norm.pos" = sites[unnorm.sites]$called.pos,
+                           "pos.clus" = sites[unnorm.sites]$pos.clus,
+                           stringsAsFactors = FALSE)
+  clus.pos.key <- as.data.frame(bind_rows(norm.key, unnorm.key))
+  row.names(clus.pos.key) <- clus.pos.key$pos.clus
+  sites$norm.pos <- clus.pos.key[as.character(sites$pos.clus), "norm.pos"]
+
+  ranges <- IRanges(start = ifelse(strand(sites) == "+",
+                                   sites$norm.pos,
+                                   sites$called.bp),
+                    end = ifelse(strand(sites) == "+",
+                                 sites$called.bp,
+                                 sites$norm.pos))
+  normalized.sites <- GRanges(seqnames = seqnames(sites),
+                              ranges = ranges,
+                              strand = strand(sites),
+                              seqinfo = seqinfo(sites))
+  mcols(normalized.sites) <- mcols(sites)
+  normalized.sites
+}
---
+++ R/normalize_multihit_clusters.R
@@ -0,0 +1,109 @@
+#' Normalize multihit cluster IDs from multiple samples.
+#'
+#' \code{normalize_multihit_clusters} will normalize multihit clusterIDs across
+#' multiple samples so that multihit sites can be identified across time points,
+#' cell types, etc.
+#'
+#' @description As the INSPIIRED pipeline calls multihits, or integration sites
+#' that can not be placed in a single location on the reference genome, it
+#' assigns multihitID's to the various locations that the integration site may
+#' exist. As each replicate is individually analyzed, multihitIDs for each
+#' replicate are different, even through they may refer to the same integration
+#' site. For this reason, normalize_multihit_clusters uses the previously
+#' assigned multihitID and genomic positions to reassign multihitIDs across
+#' multiple samples. Input for the function needs to be a GRanges object with a
+#' metadata column labeled as "multihitid".
+#'
+#' @usage
+#' normalize_multihit_clusters(multihits.gr)
+#'
+#' normalize_multihit_clusters(multihits.gr, gap = 5L, grouping = NULL)
+#'
+#' @param multihits.gr GRanges object with a column named 'multihitid'.
+#'
+#' @param gap integer designating the range to which consider sites identical.
+#'
+#' @param grouping Character, name of the column used to assign groups that will
+#' not be compared to one another. Such as 'patient'.
+#'
+#' @examples
+#' dfr <- data.frame(
+#'   "chr" = c("chr1", "chr2", "chr2", "chr3"),
+#'   "position" = c(5379927, 92775920, 2719573, 7195924),
+#'   "breakpoint" = c(5380070, 92775995, 2719450, 7195890),
+#'   "strand" = c("+", "+", "-", "-"),
+#'   "sampleName" = rep("GTSP1234-1", 4),
+#'   stringsAsFactors = FALSE)
+#'
+#' gr1 <- granges(db_to_granges(dfr))
+#' gr2 <- gr1
+#' gr1$multihitid <- c(1, 1, 2, 3)
+#' gr2$multihitid <- c(4, 4, 5, 6)
+#' gr1$patient <- rep(1, 4)
+#' gr2$patient <- rep(2, 4)
+#' gr <- normalize_intsite_positions(c(gr1, gr2))
+#'
+#' normalize_multihit_clusters(gr)
+#'
+#' # Group by patient will keep the two samples from being normalized to
+#' # eachother
+#'
+#' normalize_multihit_clusters(gr, grouping = 'patient')
+#'
+#' @author Christopher Nobles, Ph.D.
+#' @export
+
+normalize_multihit_clusters <- function(multihits.gr, gap = 5L, grouping = NULL){
+  #Multihits must be standardized and have clusterID info
+  if(is.null(grouping)){
+    multihits.gp <- GRangesList(multihits.gr)
+  }else if(grouping %in% names(mcols(multihits.gr))){
+    groups <- mcols(multihits.gr)[
+      grep(grouping, names(mcols(multihits.gr)))]
+    multihits.gr$groups <- groups[,1]
+    multihits.gp <- split(multihits.gr, multihits.gr$groups)
+  }else{
+    stop("Grouping partitioning failed. Make sure grouping is either NULL or
+         refering to the correct column in GRanges object.")
+  }
+
+  base_clus <- 0 # Start count for multihitid's
+
+  norm.multi.grl <- GRangesList(lapply(multihits.gp, function(gr){
+    key <- unique(data.frame(
+      "multihitid" = as.character(gr$multihitid),
+      "clusid" = gr$pos.clus,
+      stringsAsFactors = FALSE
+    ))
+
+    split.key <- split(key, key$clusid)
+    edgelist <- matrix(c(
+      as.character(Rle(
+        values = sapply(split.key, function(x) x$multihitid[1]),
+        lengths = sapply(split.key, nrow)
+      )),
+      unlist(sapply(split.key, function(x) x$multihitid))),
+      ncol = 2
+    )
+    graph <- graph.edgelist(edgelist, directed = FALSE)
+    norm.multi.id <- as.numeric(clusters(graph)$membership) + base_clus
+    names(norm.multi.id) <- names(clusters(graph)$membership)
+    key$norm.multihitid <- norm.multi.id[key$multihitid]
+    key$clusid <- NULL
+    key <- unique(key)
+    base_clus <<- max(norm.multi.id)
+
+    norm.key <- data.frame(
+      row.names = key$multihitid,
+      "norm.multihitid" = key$norm.multihitid
+    )
+
+    gr$norm.multihitID <- norm.key[as.character(gr$multihitid), "norm.multihitid"]
+    gr
+  }))
+
+  norm.multi.gr <- unlist(norm.multi.grl)
+  if(!is.null(grouping)){norm.multi.gr$groups <- NULL}
+  norm.multi.gr
+}
+
